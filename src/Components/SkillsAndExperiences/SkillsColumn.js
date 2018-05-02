6ba2f58ee05f5f277838d566f84a046f9536a157
Jonathan Deleon <Jondeleon856@gmail.com>
2018-05-02 00:45:30 -0400

    have progress bar dynamically rendering percentages as set in configurations section

--- src/Components/SkillsAndExperiences/SkillsColumn.js
+++ src/Components/SkillsAndExperiences/SkillsColumn.js
@@ -6,22 +6,33 @@ class SkillsColumn extends Component {
 
 
   render() {
-    const skillVar = this.props.skill;
+//     const skillVar = this.props.skill;
+//
+//     console.log("within bar style",skillVar);
+//     let tempVar;
+//     let barStyle = {};
+// // @TODO Fix This!
+//     skillVar.filter((obj)=>{
+//       console.log('within map function')
+//       console.log(obj.rating)
+//       tempVar = obj.rating;
+//
+//       barStyle = {
+//         width: `${obj.rating}%`,
+//         height: '10px',
+//         backgroundColor: 'green',
+//         zIndex: 1
+//       }
+//     })
+//     console.log("The Rating",tempVar);
 
-    console.log("within bar style",skillVar);
-// @TODO Fix This! 
-    skillVar.map((obj)=>{
-      console.log('within map function')
-      console.log(obj.rating)
-    })
-    console.log("The Rating",skillVar);
 
-    const barStyle = {
-      width: `${this.props.skill.rating}%`,
-      height: '10px',
-      backgroundColor: 'green',
-      zIndex: 1
-    }
+    // const barStyle = {
+    //   width: `${this.props.skill[0].rating}%`,
+    //   height: '10px',
+    //   backgroundColor: 'green',
+    //   zIndex: 1
+    // }
 
     return (
       <div className="Skills-Column">
@@ -30,6 +41,14 @@ class SkillsColumn extends Component {
           className="skill-category"
           id={this.props.category.split(' ').join('_')}>
           {this.props.skill.map((skill)=>{
+
+            const barStyle = {
+              width: `${skill.rating}%`,
+              height: '10px',
+              backgroundColor: 'green',
+              zIndex: 1
+            }
+
             return (
             <div className="skill-row-container">
               <div
