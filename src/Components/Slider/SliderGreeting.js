5305014397976833bd4fc9e2602867a01af5c538
Jonathan Deleon <Jondeleon856@gmail.com>
2018-05-04 00:57:16 -0400

    made header, navigation, logo, and slider greeting changes for design purposes

--- src/Components/Slider/SliderGreeting.js
+++ src/Components/Slider/SliderGreeting.js
@@ -3,11 +3,17 @@ import '../../CSS/SliderGreeting.css'
 
 class SliderGreeting extends Component {
   render() {
+    const zIndexing = {
+      zIndex:10
+    }
     return (
       <div className="Slider-Greeting">
-        <h1 className="Greeting-Container">Hello, I am Jonathan and I love
-          {/* @TODO Add Scrolling Functionality Here */}
-          <span id="scroller"> JavaScript.</span>
+        <h1 className="Greeting-Container">&nbsp;Hello, I am Jonathan and I love &nbsp;
+          <br/>
+          <span id="scroller">
+            <span class="javascript" style={zIndexing}> JavaScript.</span>
+            <span class="html" > HTML</span>
+          </span>
         </h1>
       </div>
     );
