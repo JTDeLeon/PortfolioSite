26bd9ce283d4e6b2c7278da80c35fe5dd3b9bebb
Jonathan Deleon <Jondeleon856@gmail.com>
2018-05-07 14:30:32 -0400

    styled the header section

--- src/Components/Slider/SliderGreeting.js
+++ src/Components/Slider/SliderGreeting.js
@@ -7,15 +7,22 @@ class SliderGreeting extends Component {
       zIndex:10
     }
     return (
+
       <div className="Slider-Greeting">
-        <h1 className="Greeting-Container">&nbsp;Hello, I am Jonathan and I love &nbsp;
-          <br/>
-          <span id="scroller">
-            <span class="javascript" style={zIndexing}> JavaScript.</span>
-            <span class="html" > HTML</span>
-          </span>
+        <h1 className="Greeting-Container">&nbsp;Hello, I am Jonathan and I love <span id="scroller">JavaScript.</span>
         </h1>
       </div>
+
+
+      // <div className="Slider-Greeting">
+      //   <h1 className="Greeting-Container">&nbsp;Hello, I am Jonathan and I love &nbsp;
+      //     <br/>
+      //     <span id="scroller">
+      //       <span class="javascript" style={zIndexing}> JavaScript.</span>
+      //       <span class="html" > HTML</span>
+      //     </span>
+      //   </h1>
+      // </div>
     );
   }
 }
